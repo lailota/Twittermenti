cdcbb125c2c88c8c665b6af34f4a2c5da7f80535
Laila GH <laila.guzzon@gmail.com>
2021-05-07 14:34:09 -0300

    add Secret.swift file

--- Twittermenti/Secret.swift
+++ Twittermenti/Secret.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2021 London App Brewery. All rights reserved.
 //
 
-import Foundation
+//import Foundation
+import UIKit
+
+struct Secret: Decodable {
+    let key : String
+    let secret : String
+}
+
+
+struct SecretValues {
+    static func get() -> Secret {
+        guard let url = Bundle.main.url(forResource: "Secret", withExtension: "plist") else {
+            fatalError("Could't find file 'Secret.plist'in your Bundle.")
+        }
+        do {
+            let decoder = PropertyListDecoder()
+            let data = try Data(contentsOf: url)
+            return try decoder.decode(Secret.self, from: data)
+        } catch let err {
+            fatalError(err.localizedDescription)
+        }
+    }
+}
+
+
